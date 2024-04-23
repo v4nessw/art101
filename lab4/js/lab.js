dca595107824f53fb17aa83d0877a93db0c8e710
v4nessw <vwang246@gmail.com>
2024-04-23 16:15:43 -0700

    lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,9 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Rock, Paper, Scissors
+// An implementation of the game
+// Vanessa Wang <vwang20@ucsc.edu>
+// April 2024
 
-// Constants
-
+// Loop to checking if win condition is met
+  // Print out each option: Rock, Paper, Scissors
+  // Allow the user to select Rock, Paper, or Scissors
+  // Allow computer randomly select Rock, Paper, or Scissors
+  // If user and computer selected the same thing
+    // Restart Game
+  //If they are different, choose the winner
+    // User selects rock and computer selects scissors
+      // User wins
+    // User selects paper and computer selects rock
+      // User wins
+    // User selects scissors and computer selects paper
+      // User wins
+  // If none of these conditions are met
+    // Computer wins
+    
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
