e224d9752b5391fc75dbfc774492df864954c43f
v4nessw <vwang246@gmail.com>
2024-05-05 15:01:02 -0700

    lab 8

---
+++ lab8/js/lab.js
@@ -0,0 +1,25 @@
+// index.js - Lab 8: Anon Functions and Callbacks
+// Author: Vanessa Wang
+// Date: 5 May 2024
+
+function isEven(x){
+  return (x % 2 == 0);
+}
+
+//test function
+console.log("Is 1 even? ", isEven(1));
+console.log("Is 2 even? ", isEven(2));
+
+array = [15, 32, 9, 70, 64, 734, 900]
+console.log("My array", array);
+
+var result =  array.map(isEven);
+//should return [false, true, false, true, true, true, true]
+console.log("Test of evenness of array:", results);
+
+var result = arrau/map(function(x){
+  return x ** 0.5;
+})
+//should return [[3.872983346207417, 5.656854249492381, 3, 8.366600265340756, 8, 27.09243436828813, 30]
+console.log("Squareroot of array:", result);
+
