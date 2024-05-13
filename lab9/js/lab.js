053878b1331b2ede1ebb20bca7a85a46e7c77a2e
v4nessw <vwang246@gmail.com>
2024-05-12 21:17:04 -0700

    lab 9 task x fin

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -7,33 +7,6 @@ Requirements: jQuery must be loaded for this script to work.
 // Date: 12 May 2024
 */
 
-// add button to challenge section
-$("#challenge").append("<button id='button-challenge'>Make Special</button>");
-
-// add a click listener to the challenge button
-$("#button-challenge").click(function(){
-  // now add (or subtract) the "special" class to the section
-  $("#challenge").toggleClass("special");
-});
-
-// add button to problems section
-$("#problems").append("<button id='button-problems'>Make Special</button>");
-
-// add a click listener to the problems button
-$("#button-problems").click(function(){
-  // now add (or subtract) the "special" class to the section
-  $("#problems").toggleClass("special");
-});
-
-// add button to results section
-$("#results").append("<button id='button-results'>Make Special</button>");
-
-// add a click listener to the results button
-$("#button-results").click(function(){
-  // now add (or subtract) the "special" class to the section
-  $("#results").toggleClass("special");
-});
-
 // creating the button for each special-sections
 $(".special-sections").append("<button class = 'make-special'>Make Special</button>");
 
