176e36cfa8c342ac4dab4f2c150f072be1463b76
v4nessw <vwang246@gmail.com>
2024-05-23 22:36:58 -0700

    lab12

---
+++ lab12/js/lab.js
@@ -0,0 +1,36 @@
+/*
+lab.js - This simple JavaScript/jQuery script gets a value from ann input field and outputs a modified version.
+
+Requirements: jQuery must be loaded for this script to work.
+
+Author: Vanessa Wang
+Date: 23 May 2024
+*/
+
+//Return Gryffindor, Ravenclaw, Slytherin, and Hufflepuff
+// depending on length mod 4
+function sortingHat(str) {
+    len = str.length;
+    mod = len % 4;
+    if (mod == 0) {
+        return "Gryffindor"
+    }
+    else if (mod == 1) {
+        return "Ravenclaw"
+    }
+    else if (mod == 2) {
+        return "Slytherin"
+    }
+    else if (mod == 3) {
+        return "Hufflepuff"
+    }
+}
+
+
+var myButton = document.getElementById("button");
+myButton.addEventListener("click", function() {
+    var name = document.getElementById("input").value;
+    var house = sortingHat(name);
+    newText = "<p>The Sorting Hat has sorted you into " + house + "</p>";
+    document.getElementById("output").innerHTML = newText;
+})
