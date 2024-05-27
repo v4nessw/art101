9e696d9446a610c383f0c4209b82229f1d2c755e
v4nessw <vwang246@gmail.com>
2024-05-26 22:30:34 -0700

    lab13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -7,38 +7,34 @@ Author: Vanessa Wang
 Date: 26 May 2024
 */
 
-// given a number and an object that looks like this:
-//      {3: "Fizz", 5: "Buzz", 7: "Boom"}
-// loops over the numbers and outputs the number and the matching text for factors
-function fizzBuzzBoom(maxNums, factor0bj) {
-    //iterate over all of our numbers
-    for (var num=0; num<maxNums; num++) {
-        // reset output string
-        var outputStr = "";
-        // iterate over the factors we got from the html
-        for (var factor in factor0bj) {
-            // check to see if this num is a multiple of factor
-            if (num % factor == 0) {
-                // if yes, then add the text to output string
-                outputStr += factor0bj[factor];
-            }
-        }
-        // now if we have words in outputStr, format it like this " - FizzBuzz!"
-        if (outputStr) {
-            outputStr = " - " + outputStr + "!";
-        }
-        outputToPage(num.toString() + outputStr)
+// fizzBuzz - Loops from 1 - 100 checking if each number is a multiple of 3, 5, or 7
+function fizzBuzz() {
+    let oneLongString = "";
+    // looping from 1 to 100
+    for (i=1; i <= 100; i++) {
+      oneLongString += i + " ";
+      // added a boolean to check if number is fizz buzz or boom
+      let fizzBuzzBoom = false;
+      if (i % 3 == 0){
+        oneLongString += "Fizz";
+        fizzBuzzBoom = true;
+      }
+      if (i % 5 == 0){
+        oneLongString += "Buzz";
+        fizzBuzzBoom = true;
+      }
+      if (i % 7 == 0){
+        oneLongString += "Boom";
+        fizzBuzzBoom = true;
+      }
+      // adds an exclemation mark if it fizz buzzes or booms
+      if (fizzBuzzBoom == true){
+        oneLongString += "!<br>";
+      }
+      else {
+        oneLongString+= "<br>";
+      }
     }
-}
-
-function reportError(str) {
-    outputEl.innerHTML = "<div class='error'>" + str + "</div>";
-}
-
-document.getElementById("submit").addEventListener("click", function() {
-    var max = document.getElementById("max").value;
-    console.log("max:", max)
-    if (! max) {
-        reportError("You must provide maximum");
-    }
-})
+    $("#output").html(oneLongString);
+  }
+  fizzBuzz();
