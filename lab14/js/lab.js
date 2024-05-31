b56e0c834037fba4a6072b9895fb749e90f5e699
v4nessw <vwang246@gmail.com>
2024-05-30 22:53:47 -0700

    lab14

---
+++ lab14/js/lab.js
@@ -0,0 +1,8 @@
+/*
+lab.js - This simple JavaScript/jQuery script gets a value from ann input field and outputs a modified version.
+
+Requirements: jQuery must be loaded for this script to work.
+
+Author: Vanessa Wang
+Date: May 30 2024
+*/
